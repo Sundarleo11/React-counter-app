f8fc3086d39af29c40770f824a5d9a3d7c729391
sundarleo11 <sundarrock1999@gmail.com>
2022-07-03 10:51:39 +0530

    button

---
+++ counter-app/src/compount/counter.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class counter extends Component {
+
+    render() {
+        return (<div>
+
+            <h1>Hello wolrd</h1>
+            <button>clicked</button>
+        </div>)
+
+    }
+}
+
+export default counter;
