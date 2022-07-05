0c5ebbdd3e89fd47b0c613c48706ae49b32463df
sundarleo11 <sundarrock1999@gmail.com>
2022-07-05 12:29:38 +0530

    table

---
+++ vidly/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+import Movies from './component/Movies'
+
+
+function App() {
+  return (
+    <div className="App">
+      <main className='container'>
+        <Movies />
+      </main>
+
+
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
